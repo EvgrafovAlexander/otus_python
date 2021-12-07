ecd53e20e6197ac9c5f3c4ea8e94f673c1c9ab22
EvgrafovAlexander <nemesis92@me.com>
2021-12-08 01:18:37 +0500

    # OOP-version

--- log_analyzer.py
+++ log_analyzer.py
@@ -12,7 +12,7 @@ import logging
 import os
 import re
 from datetime import datetime
-from typing import List, Tuple
+from typing import Dict, List, Tuple
 
 config = {
     "REPORT_SIZE": 1000,
@@ -25,17 +25,53 @@ DATE_PATTERN = r'\d\d\d\d\d\d\d\d'
 REF_PATTERN = r'(GET|POST).*(HTTP)'
 
 
-def main():
-    last_log = get_last_log()
-    if last_log:
+class LogAnalyzer():
+    def __init__(self, log_dir, report_dir, file_name=None, is_gz=False, is_last_log_calc=True):
+        self.log_dir = log_dir
+        self.report_dir = report_dir
+        self.file_name = file_name
+        self.is_gz = is_gz
+        self.is_last_log_calc = is_last_log_calc
+
+    def get_report(self):
+        if self.is_last_log_calc:
+            last_log = self.__get_last_log()
+            if last_log:
+                requests, full_request_time, full_request_cnt = self.__parse_log(last_log)
+                return self.__calc_stat(requests, full_request_time, full_request_cnt)
+            else:
+                logging.info('Отсутствуют логи для обработки.')
+
+    def __get_last_log(self) -> Tuple[str, bool] or None:
+        """
+        Получение наименования файла последней записи логов интерфейса
+        :return: name - наименование файла
+                 is_gz - является ли *gz расширением
+                 None - если ни одного подходящего файла не найдено
+        """
+        result = None
+        for name in os.listdir(self.log_dir):
+            found = re.search(COMMON_PATTERN, name)
+            if found:
+                date = re.search(DATE_PATTERN, found.group(0))
+                if date:
+                    date = datetime.strptime(date.group(0), "%Y%m%d")
+                    if result:
+                        if date > result[0]:
+                            result = date, name, name[-2:] == 'gz'
+                    else:
+                        result = date, name, name[-2:] == 'gz'
+        return result
+
+    def __parse_log(self, last_log) -> Tuple[Dict[str, List[float]], int, int]:
         requests = dict()
         full_request_time, full_request_cnt, error_cnt = 0, 0, 0
         _, filename, is_gz = last_log
-        read_params = config['LOG_DIR'] + '/' + filename, 'rb'
+        read_params = self.log_dir + '/' + filename, 'rb'
         open_func = gzip.open(*read_params) if is_gz else open(*read_params)
         with open_func as log_file:
             for line in log_file:
-                line_info = line_parse(line)
+                line_info = self.__line_parse(line)
                 if line_info:
                     request, request_time = line_info
                     if request not in requests:
@@ -46,104 +82,83 @@ def main():
                     full_request_cnt += 1
                 else:
                     error_cnt += 1
-        log_stat = calc_stat(requests, full_request_time, full_request_cnt)
-        save_report(log_stat)
-    else:
-        logging.info('Отсутствуют логи для обработки.')
+        return requests, full_request_time, full_request_cnt
 
+    @staticmethod
+    def __line_parse(line: bytes) -> Tuple[str, float] or None:
+        """
+        Парсинг строки лога
+        :line: строка лога
 
-def get_last_log() -> Tuple[str, bool] or None:
-    """
-    Получение наименования файла последней записи логов интерфейса
-    :return: name - наименование файла
-             is_gz - является ли *gz расширением
-             None - если ни одного подходящего файла не найдено
-    """
-    result = None
-    for name in os.listdir(config['LOG_DIR']):
-        found = re.search(COMMON_PATTERN, name)
+        :return: request - http-запрос
+                 request_time - длительность обработки запроса
+                 None - если не удалось распознать строку
+        """
+        line = line.decode('utf-8')
+        found = re.search(REF_PATTERN, line)
         if found:
-            date = re.search(DATE_PATTERN, found.group(0))
-            if date:
-                date = datetime.strptime(date.group(0), "%Y%m%d")
-                if result:
-                    if date > result[0]:
-                        result = date, name, name[-2:] == 'gz'
-                else:
-                    result = date, name, name[-2:] == 'gz'
-    return result
+            request = found.group(0).split()[1]
+            request_time = float(line.split()[-1])
+            return request, request_time
+        return None
+
+    def __calc_stat(self, requests: dict, full_time: float, full_cnt: int):
+        """
+        Вычисление статистических показателей
+        и подготовка результирующих данных
+        :requests: словарь вида url-запрос: список request_time
+        :full_time: общая длительность выполнения запросов
+        :full_cnt: общее количество выполненных запросов
+
+        :return: список с показателями по каждому url
+        """
+        stat = []
+        for request, times in requests.items():
+            time_sum = sum(times)
+            stat.append(
+                {'url': request,
+                 'count': len(times),
+                 'count_perc': round(100 * len(times) / full_cnt, 3),
+                 'time_sum': round(time_sum, 3),
+                 'time_perc': round(100 * time_sum / full_time, 3),
+                 'time_avg': round(time_sum / len(times), 3),
+                 'time_max': round(max(times), 3),
+                 'time_med': round(self.__get_median(times), 3)}
+            )
+        return stat
+
+    @staticmethod
+    def __get_median(values: List[float]) -> float:
+        """
+        Получение медианного значения
+        :values: список request_time
+
+        :return: медианное значение
+        """
+        values.sort()
+        if len(values) % 2:
+            return values[len(values) // 2]
+        else:
+            first = len(values) // 2
+            return (values[first] + values[first - 1]) / 2
+
+    def save_report(self, report: List[dict]) -> None:
+        """
+        Запись отчёта в html
+        :report: сформированный отчёт
+
+        :return: None
+        """
+        template = open(self.report_dir + '/report.html').read()
+        report = re.sub('\$table_json', json.dumps(report), template)
+        with open(self.report_dir + '/' + 'report_final.html', 'w') as report_file:
+            report_file.write(report)
 
 
-def line_parse(line: bytes) -> Tuple[str, float] or None:
-    """
-    Парсинг строки лога
-    :line: строка лога
-
-    :return: request - http-запрос
-             request_time - длительность обработки запроса
-             None - если не удалось распознать строку
-    """
-    line = line.decode('utf-8')
-    found = re.search(REF_PATTERN, line)
-    if found:
-        request = found.group(0).split()[1]
-        request_time = float(line.split()[-1])
-        return request, request_time
-    return None
-
-
-def calc_stat(requests: dict, full_time: float, full_cnt: int):
-    """
-    Вычисление статистических показателей
-    и подготовка результирующих данных
-    :requests: словарь вида url-запрос: список request_time
-    :full_time: общая длительность выполнения запросов
-    :full_cnt: общее количество выполненных запросов
-
-    :return: список с показателями по каждому url
-    """
-    stat = []
-    for request, times in requests.items():
-        time_sum = sum(times)
-        stat.append(
-            {'url': request,
-             'count': len(times),
-             'count_perc': round(100 * len(times) / full_cnt, 3),
-             'time_sum': round(time_sum, 3),
-             'time_perc': round(100 * time_sum / full_time, 3),
-             'time_avg': round(time_sum / len(times), 3),
-             'time_max': round(max(times), 3),
-             'time_med': round(get_median(times), 3)}
-        )
-    return stat
-
-
-def get_median(values: List[float]) -> float:
-    """
-    Получение медианного значения
-    :values: список request_time
-
-    :return: медианное значение
-    """
-    values.sort()
-    if len(values) % 2:
-        return values[len(values) // 2]
-    else:
-        first = len(values) // 2
-        return (values[first] + values[first - 1]) / 2
-
-
-def save_report(report: List[dict]) -> None:
-    """
-    Запись отчёта в html
-    :report: сформированный отчёт
-
-    :return: None
-    """
-    template = open(config["REPORT_DIR"] + '/report.html').read()
-    report = re.sub('\$table_json', json.dumps(report), template)
-    with open(config["REPORT_DIR"] + '/' + 'report_final.html', 'w') as report_file:
-        report_file.write(report)
+def main():
+    analyzer = LogAnalyzer(config["LOG_DIR"], config["REPORT_DIR"])
+    report = analyzer.get_report()
+    analyzer.save_report(report)
 
 
 if __name__ == "__main__":
