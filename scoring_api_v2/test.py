9cd808f1edfaa170ec621cdc8dcdab8bec2422a7
EvgrafovAlexander <nemesis92@me.com>
2022-01-25 00:51:26 +0500

    # pytest form

--- scoring_api_v2/test.py
+++ scoring_api_v2/test.py
@@ -1,21 +1,102 @@
-import unittest
+import pytest
+import hashlib
+import datetime
 
 import api
 
 
-class TestSuite(unittest.TestCase):
-    def setUp(self):
-        self.context = {}
-        self.headers = {}
-        self.store = None
+context = {}
+headers = {}
+store = None
 
-    def get_response(self, request):
-        return api.method_handler({"body": request, "headers": self.headers}, self.context, self.store)
+test_bad_auth_data = [
+    ({"account": "horns&hoofs", "login": "h&f", "method": "online_score", "token": "", "arguments": {}}),
+    ({"account": "horns&hoofs", "login": "h&f", "method": "online_score", "token": "sdd", "arguments": {}}),
+    ({"account": "horns&hoofs", "login": "admin", "method": "online_score", "token": "", "arguments": {}})
+]
 
-    def test_empty_request(self):
-        _, code = self.get_response({})
-        self.assertEqual(api.INVALID_REQUEST, code)
+test_invalid_method_request_data = [
+    ({"account": "horns&hoofs", "login": "h&f", "method": "online_score"}),
+    ({"account": "horns&hoofs", "login": "h&f", "arguments": {}}),
+    ({"account": "horns&hoofs", "method": "online_score", "arguments": {}})
+]
+
+test_invalid_score_request_data = [
+    ({}),
+    ({"phone": "79175002040"}),
+    ({"phone": "89175002040", "email": "stupnikov@otus.ru"}),
+    ({"phone": "79175002040", "email": "stupnikovotus.ru"}),
+    ({"phone": "79175002040", "email": "stupnikov@otus.ru", "gender": -1}),
+    ({"phone": "79175002040", "email": "stupnikov@otus.ru", "gender": "1"}),
+    ({"phone": "79175002040", "email": "stupnikov@otus.ru", "gender": 1, "birthday": "01.01.1890"}),
+    ({"phone": "79175002040", "email": "stupnikov@otus.ru", "gender": 1, "birthday": "XXX"}),
+    ({"phone": "79175002040", "email": "stupnikov@otus.ru", "gender": 1, "birthday": "01.01.2000", "first_name": 1}),
+    ({"phone": "79175002040", "email": "stupnikov@otus.ru", "gender": 1, "birthday": "01.01.2000",
+      "first_name": "s", "last_name": 2}),
+    ({"phone": "79175002040", "birthday": "01.01.2000", "first_name": "s"}),
+    ({"email": "stupnikov@otus.ru", "gender": 1, "last_name": 2})
+]
+
+test_ok_score_arguments_data = [
+    ({"phone": "79175002040", "email": "stupnikov@otus.ru"}),
+    ({"phone": 79175002040, "email": "stupnikov@otus.ru"}),
+    ({"gender": 1, "birthday": "01.01.2000", "first_name": "a", "last_name": "b"}),
+    ({"gender": 0, "birthday": "01.01.2000"}),
+    ({"gender": 2, "birthday": "01.01.2000"}),
+    ({"first_name": "a", "last_name": "b"}),
+    ({"phone": "79175002040", "email": "stupnikov@otus.ru", "gender": 1, "birthday": "01.01.2000",
+      "first_name": "a", "last_name": "b"})
+]
+
+
+def set_valid_auth(request):
+    if request.get("login") == api.ADMIN_LOGIN:
+        request["token"] = hashlib.sha512(datetime.datetime.now().strftime("%Y%m%d%H").encode('utf-8')
+                                          + api.ADMIN_SALT.encode('utf-8')).hexdigest()
+    else:
+        msg = request.get("account", "") + request.get("login", "") + api.SALT
+        request["token"] = hashlib.sha512(msg.encode('utf-8')).hexdigest()
+
+
+def test_empty_request():
+    request = {}
+    _, code = api.method_handler({"body": request, "headers": headers}, context, store)
+    assert api.INVALID_REQUEST == code
+
+
+@pytest.mark.parametrize("request_data", test_bad_auth_data)
+def test_bad_auth(request_data):
+    _, code = api.method_handler({"body": request_data, "headers": headers}, context, store)
+    assert api.FORBIDDEN == code
+
+
+@pytest.mark.parametrize("request_data", test_invalid_method_request_data)
+def test_invalid_method_request(request_data):
+    set_valid_auth(request_data)
+    response, code = api.method_handler({"body": request_data, "headers": headers}, context, store)
+    assert api.INVALID_REQUEST == code
+    assert len(response)
+
+
+@pytest.mark.parametrize("arguments_data", test_invalid_score_request_data)
+def test_invalid_score_request(arguments_data):
+    request = {"account": "horns&hoofs", "login": "h&f", "method": "online_score", "arguments": arguments_data}
+    set_valid_auth(request)
+    response, code = api.method_handler({"body": request, "headers": headers}, context, store)
+    assert api.INVALID_REQUEST == code
+    assert len(response)
+
+
+@pytest.mark.parametrize("arguments_data", test_ok_score_arguments_data)
+def test_ok_score_request(arguments_data):
+    request = {"account": "horns&hoofs", "login": "h&f", "method": "online_score", "arguments": arguments_data}
+    set_valid_auth(request)
+    response, code = api.method_handler({"body": request, "headers": headers}, context, store)
+    score = response.get("score")
+    assert api.OK == code
+    assert isinstance(score, (int, float)) and score >= 0
+    assert context["has"] == sorted(arguments_data.keys())
 
 
 if __name__ == "__main__":
-    unittest.main()
+    pytest.main()
