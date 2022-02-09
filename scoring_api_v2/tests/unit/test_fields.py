dceb3100af8e0d6e8013cc3e23fece3388e8645c
EvgrafovAlexander <nemesis92@me.com>
2022-02-09 21:52:26 +0500

    # fix test_fields

--- scoring_api_v2/tests/unit/test_fields.py
+++ scoring_api_v2/tests/unit/test_fields.py
@@ -4,136 +4,116 @@ import api
 from api import ValidationError
 
 
-class TestAbstractField:
-    field = api.AbstractField(required=True, nullable=False)
-
-    def test_ok(self):
-        self.field = 'text'
-        assert self.field == 'text'
-
-    def test_required_with_none(self):
-        with pytest.raises(ValidationError):
-            self.field = None
-
-
 class TestCharField:
     field = api.CharField(required=True, nullable=False)
 
     def test_ok(self):
-        self.field = 'text'
-        assert self.field == 'text'
+        assert self.field.validate('text') is None
 
     def test_incorrect_type(self):
         with pytest.raises(ValidationError):
-            self.field = 1
+            self.field.validate(1)
 
 
 class TestArgumentsField:
     field = api.ArgumentsField(required=True, nullable=False)
 
     def test_ok(self):
-        self.field = {}
-        assert self.field == {}
+        assert self.field.validate({}) is None
 
     def test_incorrect_type(self):
         with pytest.raises(ValidationError):
-            self.field = 'invalid_type'
+            self.field.validate('invalid_type')
 
 
 class TestEmailField:
     field = api.EmailField(required=True, nullable=False)
 
     def test_ok(self):
-        self.field = 'test@mail.com'
-        assert self.field == 'test@mail.com'
+        assert self.field.validate('test@mail.com') is None
 
     def test_incorrect_form(self):
         with pytest.raises(ValidationError):
-            self.field = 'testmail.com'
+            self.field.validate('testmail.com')
 
 
 class TestPhoneField:
     field = api.PhoneField(required=True, nullable=False)
 
     def test_ok(self):
-        self.field = '79210010033'
-        assert self.field == '79210010033'
+        assert self.field.validate('79210010033') is None
 
     def test_incorrect_start_num(self):
         with pytest.raises(ValidationError):
-            self.field = '89232344123'
+            self.field.validate('89232344123')
 
     def test_incorrect_length(self):
         with pytest.raises(ValidationError):
-            self.field = '7923223'
+            self.field.validate('7923223')
 
     def test_incorrect_type(self):
         with pytest.raises(ValidationError):
-            self.field = 2.5
+            self.field.validate(2.5)
 
 
 class TestDateField:
     field = api.DateField(required=True, nullable=False)
 
     def test_ok(self):
-        self.field = '21.01.1970'
-        assert self.field == '21.01.1970'
+        assert self.field.validate('21.01.1970') is None
 
     def test_incorrect_value(self):
         with pytest.raises(ValidationError):
-            self.field = '21011970'
+            self.field.validate('21011970')
 
 
 class TestBirthDayField:
     field = api.BirthDayField(required=True, nullable=False)
 
     def test_ok(self):
-        self.field = '21.01.1970'
-        assert self.field == '21.01.1970'
+        assert self.field.validate('21.01.1970') is None
 
     def test_incorrect_value(self):
         with pytest.raises(ValidationError):
-            self.field = '21011970'
+            self.field.validate('21011970')
 
     def test_incorrect_age(self):
         with pytest.raises(ValidationError):
-            self.field = '21.01.1800'
+            self.field.validate('21.01.1800')
 
 
 class TestGenderField:
     field = api.GenderField(required=True, nullable=False)
 
     def test_ok(self):
-        self.field = 0
-        assert self.field == 0
+        assert self.field.validate(0) is None
 
     def test_incorrect_type(self):
         with pytest.raises(ValidationError):
-            self.field = '1'
+            self.field.validate('1')
 
     def test_incorrect_value(self):
         with pytest.raises(ValidationError):
-            self.field = 3
+            self.field.validate(3)
 
 
 class TestClientIDsField:
     field = api.ClientIDsField(required=True, nullable=False)
 
     def test_ok(self):
-        self.field = [1, 2, 3, 4]
-        assert self.field == [1, 2, 3, 4]
+        assert self.field.validate([1, 2, 3, 4]) is None
 
     def test_incorrect_type(self):
         with pytest.raises(ValidationError):
-            self.field = '1'
+            self.field.validate('1')
 
     def test_empty_list(self):
         with pytest.raises(ValidationError):
-            self.field = []
+            self.field.validate([])
 
     def test_incorrect_value(self):
         with pytest.raises(ValidationError):
-            self.field = [1, '2', 3, 'a']
+            self.field.validate([1, '2', 3, 'a'])
 
 
 if __name__ == "__main__":
