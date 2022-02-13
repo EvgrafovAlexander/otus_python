55b878cfb05b33e17d751b3b97279a16520d62db
EvgrafovAlexander <nemesis92@me.com>
2022-02-13 22:22:15 +0500

    # Изменена функция get, добавлены тесты store

--- scoring_api_v2/store.py
+++ scoring_api_v2/store.py
@@ -1,27 +1,43 @@
-import redis
 import json
+import logging
+import redis
+import time
 
 
 class Store:
-    def __init__(self, host='localhost', port=6379, db=0):
+    def __init__(self, host='localhost', port=6379, db=0, sock_timeout=1, sock_conn_timeout=1,
+                 retry_counts=3, retry_timeout=1):
         self._store = redis.Redis(host=host,
                                   port=port,
-                                  db=db)
+                                  db=db,
+                                  socket_connect_timeout=sock_conn_timeout,
+                                  socket_timeout=sock_timeout)
+        self.retry_counts = retry_counts
+        self.retry_timeout = retry_timeout
 
     def cache_get(self, key):
-        val = self._store.get(key)
-        if val:
-            return json.loads(val)
-        return None
-
-    def cache_set(self, key, value, storage_time):
-        self._store.set(key, json.dumps(value), ex=storage_time)
-
-    # TODO: Доработать метод
-    def get(self, key):
         try:
             val = self._store.get(key)
             if val:
                 return json.loads(val)
-        except:
-            raise redis.exceptions.RedisError
+        except Exception as e:
+            logging.exception("Get value from Redis error: %s" % e)
+            return None
+
+    def cache_set(self, key, value, storage_time):
+        try:
+            self._store.set(key, json.dumps(value), ex=storage_time)
+        except Exception as e:
+            logging.exception("Set value to Redis error: %s" % e)
+
+    def get(self, key):
+        for i in range(1, self.retry_counts+1):
+            try:
+                val = self._store.get(key)
+                if val:
+                    return json.loads(val)
+            except:
+                logging.exception("Invalid connection, retry: %i" % i)
+                time.sleep(self.retry_timeout)
+                if i == self.retry_counts:
+                    raise ConnectionError
---
+++ scoring_api_v2/tests/integration/test_store.py
@@ -0,0 +1,24 @@
+import pytest
+
+from redis.exceptions import ConnectionError
+from store import Store
+
+
+@pytest.fixture(autouse=True)
+def redis_init(tmpdir):
+    store = Store()
+    yield store
+    store._store.close()
+
+
+def test_get_unknown_key(redis_init):
+    assert redis_init.get('1') is None
+
+
+def test_readwrite(redis_init):
+    redis_init.cache_set('key', 42, 60)
+    assert redis_init.get('key') == 42
+
+
+if __name__ == "__main__":
+    pytest.main()
