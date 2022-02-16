ada46e0f8b4074f7128c5f10d68d8d24426e6d08
EvgrafovAlexander <nemesis92@me.com>
2022-02-16 21:12:23 +0500

    # add template

--- hasker/posts/views.py
+++ hasker/posts/views.py
@@ -8,7 +8,4 @@ from .models import Post
 
 def index(request):
     latest = Post.objects.order_by('-created_date')[:10]
-    output = []
-    for item in latest:
-        output.append(item.text)
-    return HttpResponse('\n'.join(output))
+    return render(request, "index.html", {"posts": latest})
