f257e0ef05315ef5bae69ef85427853d6415a592
EvgrafovAlexander <nemesis92@me.com>
2022-02-18 00:43:25 +0500

    # add question_detail

--- hasker/posts/urls.py
+++ hasker/posts/urls.py
@@ -4,8 +4,11 @@ from . import views
 
 
 urlpatterns = [
+    # name - наименование шаблона в templates
     # /
     path("", views.index, name="index"),
+    path("questions/<int:pk>/", views.question_detail, name="question_detail"),
+
     # профиль пользователя
     #path("<str:username>/", views.profile, name="profile")
 
--- hasker/posts/views.py
+++ hasker/posts/views.py
@@ -1,5 +1,5 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 from .models import User, Question
 
@@ -9,3 +9,8 @@ from .models import User, Question
 def index(request):
     latest = Question.objects.order_by('-pub_date')[:10]
     return render(request, "index.html", {"questions": latest})
+
+
+def question_detail(request, pk):
+    question = get_object_or_404(Question, pk=pk)
+    return render(request, "question_detail.html", {"question": question})
