fdcab4c1e79097d459cb3d2a5e7b8e67cf3c7eb9
EvgrafovAlexander <nemesis92@me.com>
2022-02-22 02:17:19 +0500

    # add login template

--- hasker/hasker/settings.py
+++ hasker/hasker/settings.py
@@ -128,3 +128,6 @@ STATIC_ROOT = os.path.join(BASE_DIR, "static")
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# URL redirecting after a successful authentication
+LOGIN_REDIRECT_URL = 'index'
