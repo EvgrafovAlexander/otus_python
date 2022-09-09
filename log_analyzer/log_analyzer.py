74f291bed6b12d241073426658b621e299348fcd
EvgrafovAlexander <35798672+EvgrafovAlexander@users.noreply.github.com>
2022-09-09 18:23:49 +0500

    update

--- log_analyzer/log_analyzer.py
+++ log_analyzer/log_analyzer.py
@@ -6,7 +6,8 @@
 #                     '$status $body_bytes_sent "$http_referer" '
 #                     '"$http_user_agent" "$http_x_forwarded_for" "$http_X_REQUEST_ID" "$http_X_RB_USER" '
 #                     '$request_time';
-import argparse, configparser
+import argparse
+import configparser
 import gzip
 import json
 import logging
@@ -23,13 +24,13 @@ default_config = {
     "REPORT_DIR": "./reports/",
     "LOG_DIR": "./log/",
     "ERROR_PERC_LIMIT": 50,
-    "LOG_FILE_PATH": None
+    "LOG_FILE_PATH": None,
 }
 
-Log = namedtuple('Log', 'date name path is_gz')
+Log = namedtuple("Log", "date name path is_gz")
 
-COMMON_PATTERN = r'^nginx-access-ui\.log-(?P<date>\d{8})(\.gz)?$'
-REF_PATTERN = r'(GET|POST).*(HTTP)'
+COMMON_PATTERN = r"^nginx-access-ui\.log-(?P<date>\d{8})(\.gz)?$"
+REF_PATTERN = r"(GET|POST).*(HTTP)"
 
 
 def get_report(log: Log) -> Tuple[List[dict], float]:
@@ -70,7 +71,7 @@ def get_last_log(log_dir: str) -> namedtuple or None:
             try:
                 date = datetime.strptime(date, "%Y%m%d")
             except ValueError:
-                logging.error('Невозможно извлечь дату: %s', date)
+                logging.error("Невозможно извлечь дату: %s", date)
                 continue
 
             if not last_log or date > last_log.date:
@@ -90,9 +91,9 @@ def parse_log(log: Log, parse_func: Callable) -> Tuple[Dict[str, List[float]], i
              full_cnt - общее количество выполненных запросов
              error_cnt - общее количество ошибок распознавания
     """
-    requests = dict()
+    requests = {}
     full_request_time, full_request_cnt, error_cnt = 0, 0, 0
-    read_params = os.path.join(log.path, log.name), 'rb'
+    read_params = os.path.join(log.path, log.name), "rb"
     open_func = gzip.open(*read_params) if log.is_gz else open(*read_params)
     with open_func as log_file:
         for line in log_file:
@@ -113,12 +114,12 @@ def parse_log(log: Log, parse_func: Callable) -> Tuple[Dict[str, List[float]], i
 def is_already_analyzed(log: Log, report_dir: str) -> bool:
     """
     Проверка на существование отчёта по данному логу
-    :report_dir: информация о логе
-    :report_dir: директория хранения отчётов
+    :param log: информация о логе
+    :param report_dir: директория хранения отчётов
 
     :return: флаг наличия отчёта
     """
-    path = os.path.join(report_dir, 'report-' + log.date.strftime("%Y.%m.%d") + '.html')
+    path = os.path.join(report_dir, "report-" + log.date.strftime("%Y.%m.%d") + ".html")
     return os.path.exists(path)
 
 
@@ -130,8 +131,8 @@ def parse_line(line: bytes) -> Tuple[str, float] or None:
     :return: request - http-запрос
              request_time - длительность обработки запроса
              None - если не удалось распознать строку
-        """
-    line = line.decode('utf-8')
+    """
+    line = line.decode("utf-8")
     found = re.search(REF_PATTERN, line)
     if found:
         request = found.group(0).split()[1]
@@ -153,20 +154,22 @@ def calc_stat(requests: dict, full_time: float, full_cnt: int) -> List[dict]:
     stat = []
 
     if not full_cnt:
-        logging.info('Не найдено ни одной записи в логе. Анализ остановлен.')
+        logging.info("Не найдено ни одной записи в логе. Анализ остановлен.")
         return stat
 
     for request, times in requests.items():
         time_sum = sum(times)
         stat.append(
-            {'url': request,
-             'count': len(times),
-             'count_perc': round(100 * len(times) / full_cnt, 3),
-             'time_sum': round(time_sum, 3),
-             'time_perc': round(100 * time_sum / full_time, 3),
-             'time_avg': round(time_sum / len(times), 3),
-             'time_max': round(max(times), 3),
-             'time_med': round(statistics.median(times), 3)}
+            {
+                "url": request,
+                "count": len(times),
+                "count_perc": round(100 * len(times) / full_cnt, 3),
+                "time_sum": round(time_sum, 3),
+                "time_perc": round(100 * time_sum / full_time, 3),
+                "time_avg": round(time_sum / len(times), 3),
+                "time_max": round(max(times), 3),
+                "time_med": round(statistics.median(times), 3),
+            }
         )
     return stat
 
@@ -181,12 +184,12 @@ def save_report(log: namedtuple, report: List[dict], report_dir: str, report_siz
 
     :return: None
     """
-    report.sort(key=lambda x: x['time_sum'], reverse=True)
-    with open(os.path.join(report_dir, 'report.html')) as template:
+    report.sort(key=lambda x: x["time_sum"], reverse=True)
+    with open(os.path.join(report_dir, "report.html")) as template:
         template = template.read()
-    report = re.sub('\$table_json', json.dumps(report[:report_size]), template)
+    report = re.sub(r"\$table_json", json.dumps(report[:report_size]), template)
 
-    with open(os.path.join(report_dir, 'report-' + log.date.strftime("%Y.%m.%d") + '.html'), 'w') as report_file:
+    with open(os.path.join(report_dir, "report-" + log.date.strftime("%Y.%m.%d") + ".html"), "w") as report_file:
         report_file.write(report)
 
 
@@ -210,45 +213,47 @@ def set_config(args, conf_default: dict) -> dict:
             conf = configparser.ConfigParser()
             conf.read(args.config_file_path)
 
-            report_size = int(conf['DEFAULT']['REPORT_SIZE'])
-            report_dir = conf['DEFAULT']['REPORT_DIR']
-            log_dir = conf['DEFAULT']['LOG_DIR']
-            err_perc_limit = float(conf['DEFAULT']['ERROR_PERC_LIMIT'])
-            log_file_path = conf['DEFAULT']['LOG_FILE_PATH']
+            report_size = int(conf["DEFAULT"]["REPORT_SIZE"])
+            report_dir = conf["DEFAULT"]["REPORT_DIR"]
+            log_dir = conf["DEFAULT"]["LOG_DIR"]
+            err_perc_limit = float(conf["DEFAULT"]["ERROR_PERC_LIMIT"])
+            log_file_path = conf["DEFAULT"]["LOG_FILE_PATH"]
 
         except Exception as e:
-            logging.exception('Возникло исключение при чтении конфигурационного файла %s, %s', type(e), e.args)
+            logging.exception("Возникло исключение при чтении конфигурационного файла %s, %s", type(e), e.args)
             sys.exit(1)
     else:
         # config из параметров
-        report_size = int(args.report_size) if args.report_size else conf_default['REPORT_SIZE']
-        report_dir = args.report_dir if args.report_dir else conf_default['REPORT_DIR']
-        log_dir = args.log_dir if args.log_dir else conf_default['LOG_DIR']
-        err_perc_limit = float(args.err_perc_limit) if args.err_perc_limit else conf_default['ERROR_PERC_LIMIT']
-        log_file_path = args.log_file_path if args.log_file_path else conf_default['LOG_FILE_PATH']
+        report_size = int(args.report_size) if args.report_size else conf_default["REPORT_SIZE"]
+        report_dir = args.report_dir if args.report_dir else conf_default["REPORT_DIR"]
+        log_dir = args.log_dir if args.log_dir else conf_default["LOG_DIR"]
+        err_perc_limit = float(args.err_perc_limit) if args.err_perc_limit else conf_default["ERROR_PERC_LIMIT"]
+        log_file_path = args.log_file_path if args.log_file_path else conf_default["LOG_FILE_PATH"]
 
-    logging.info('Директория чтения логов: %s', log_dir)
-    logging.info('Директория записи отчётов: %s', report_dir)
-    logging.info('Предельный размер отчёта: %i', report_size)
-    logging.info('Предельный %% ошибок: %.1f', err_perc_limit)
+    logging.info("Директория чтения логов: %s", log_dir)
+    logging.info("Директория записи отчётов: %s", report_dir)
+    logging.info("Предельный размер отчёта: %i", report_size)
+    logging.info("Предельный %% ошибок: %.1f", err_perc_limit)
 
-    config = {'LOG_DIR': log_dir,
-              'REPORT_DIR': report_dir,
-              'REPORT_SIZE': report_size,
-              'ERROR_PERC_LIMIT': err_perc_limit,
-              'LOG_FILE_PATH': log_file_path}
+    config = {
+        "LOG_DIR": log_dir,
+        "REPORT_DIR": report_dir,
+        "REPORT_SIZE": report_size,
+        "ERROR_PERC_LIMIT": err_perc_limit,
+        "LOG_FILE_PATH": log_file_path,
+    }
 
     return config
 
 
 def get_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument('-log', "--log_dir", type=str, help="Log dir path example: ./log/")
-    parser.add_argument('-rep', "--report_dir", type=str, help="Reports dir path example: ./reports/")
-    parser.add_argument('-size', "--report_size", type=int, help="Report size example: 1000")
-    parser.add_argument('-err', "--err_perc_limit", type=float, help="Error limit % example: 50.0")
-    parser.add_argument('-path', "--log_file_path", type=str, help="Logging file path: ./log_analyzer.log")
-    parser.add_argument('-config', "--config_file_path", type=str, help="Configuration file path: ./config.ini")
+    parser.add_argument("-log", "--log_dir", type=str, help="Log dir path example: ./log/")
+    parser.add_argument("-rep", "--report_dir", type=str, help="Reports dir path example: ./reports/")
+    parser.add_argument("-size", "--report_size", type=int, help="Report size example: 1000")
+    parser.add_argument("-err", "--err_perc_limit", type=float, help="Error limit % example: 50.0")
+    parser.add_argument("-path", "--log_file_path", type=str, help="Logging file path: ./log_analyzer.log")
+    parser.add_argument("-config", "--config_file_path", type=str, help="Configuration file path: ./config.ini")
     args = parser.parse_args()
     return args
 
@@ -258,29 +263,34 @@ def main():
 
     config = set_config(args, default_config)
 
-    logging.basicConfig(format='[%(asctime)s] %(levelname).1s:%(message)s',
-                        level=logging.DEBUG,
-                        datefmt='%Y.%m.%d %H:%M:%S',
-                        filename=config['LOG_FILE_PATH'])
+    logging.basicConfig(
+        format="[%(asctime)s] %(levelname).1s:%(message)s",
+        level=logging.DEBUG,
+        datefmt="%Y.%m.%d %H:%M:%S",
+        filename=config["LOG_FILE_PATH"],
+    )
     try:
-        log = get_last_log(config['LOG_DIR'])
+        log = get_last_log(config["LOG_DIR"])
 
         if not log:
-            logging.info('Отсутствуют логи для обработки. Анализ остановлен.')
-        elif is_already_analyzed(log, config['REPORT_DIR']):
-            logging.info('Отчёт по последнему логу уже существует. Анализ остановлен.')
+            logging.info("Отсутствуют логи для обработки. Анализ остановлен.")
+        elif is_already_analyzed(log, config["REPORT_DIR"]):
+            logging.info("Отчёт по последнему логу уже существует. Анализ остановлен.")
         else:
             report, error_perc = get_report(log)
 
-            if error_perc > config['ERROR_PERC_LIMIT']:
-                logging.info('Превышение порога ошибок парсинга: %s %% > %s %%. Анализ остановлен',
-                             round(error_perc, 1), config['ERROR_PERC_LIMIT'])
+            if error_perc > config["ERROR_PERC_LIMIT"]:
+                logging.info(
+                    "Превышение порога ошибок парсинга: %s %% > %s %%. Анализ остановлен",
+                    round(error_perc, 1),
+                    config["ERROR_PERC_LIMIT"],
+                )
 
             if report:
-                save_report(log, report, config['REPORT_DIR'], config['REPORT_SIZE'])
+                save_report(log, report, config["REPORT_DIR"], config["REPORT_SIZE"])
 
     except Exception as e:
-        logging.exception('Необработанное исключение %s, %s', type(e), e.args)
+        logging.exception("Необработанное исключение %s, %s", type(e), e.args)
 
 
 if __name__ == "__main__":
