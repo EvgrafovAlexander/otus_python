4697665add50746d9185a3b7ab60e69526c6e48c
EvgrafovAlexander <35798672+EvgrafovAlexander@users.noreply.github.com>
2022-10-19 00:16:21 +0500

    add question and tag models

--- hasker/hasker/hasker/posts/admin.py
+++ hasker/hasker/hasker/posts/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin  # noqa
 
+from .models import Question, Tag
+
+
 # Register your models here.
+class QuestionAdmin(admin.ModelAdmin):
+    list_display = ("title", "text", "pub_date", "author")
+    list_filter = ["pub_date"]
+    search_fields = ["title"]
+
+
+class TagAdmin(admin.ModelAdmin):
+    list_display = ("name",)
+    list_filter = ["name"]
+    search_fields = ["name"]
+
+
+admin.site.register(Question, QuestionAdmin)
+admin.site.register(Tag, TagAdmin)
--- hasker/hasker/hasker/posts/models.py
+++ hasker/hasker/hasker/posts/models.py
@@ -1,3 +1,23 @@
-from django.db import models  # noqa
+from django.conf import settings
+from django.db import models
+
+User = settings.AUTH_USER_MODEL
+
 
 # Create your models here.
+class Tag(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+
+class Question(models.Model):
+    title = models.TextField()
+    text = models.TextField()
+    pub_date = models.DateTimeField("published date", auto_now_add=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="questions")
+    tags = models.ManyToManyField(Tag)
+
+    def __str__(self):
+        return self.title
