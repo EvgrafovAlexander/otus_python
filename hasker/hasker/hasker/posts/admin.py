61fcd5d61b0d2a6a0a3e3bcb41beaf7f284bbb48
EvgrafovAlexander <nemesis92@me.com>
2022-10-22 11:53:29 +0500

    add tags admin

--- hasker/hasker/hasker/posts/admin.py
+++ hasker/hasker/hasker/posts/admin.py
@@ -4,16 +4,27 @@ from .models import Answer, Question, Tag
 
 
 # Register your models here.
+class PostInline(admin.TabularInline):
+    model = Question.tags.through
+
+
 class QuestionAdmin(admin.ModelAdmin):
-    list_display = ("title", "text", "pub_date", "author")
+    list_display = ("title", "text", "pub_date", "author", "tags_list")
     list_filter = ["pub_date"]
     search_fields = ["title"]
 
+    def tags_list(self, obj):
+        return "\n".join([t.name for t in obj.tags.all()])
+
 
 class TagAdmin(admin.ModelAdmin):
+    model = Tag
     list_display = ("name",)
     list_filter = ["name"]
     search_fields = ["name"]
+    inlines = [
+        PostInline,
+    ]
 
 
 class TagAnswer(admin.ModelAdmin):
